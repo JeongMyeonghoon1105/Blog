cf3eda84fc0e1346d168c16abf1f1aa69d81ea68
JeongMyeonghoon1105 <wjdaudgns1105@naver.com>
2022-02-22 11:43:26 +0900

    Faster responsive

--- js/header.js
+++ js/header.js
@@ -1,24 +1,3 @@
-/* Menu Icon */
-var menuIcon = document.getElementById('menu-icon');
-/* Menu Icon Container */
-var hiddenMenu = document.getElementById('hidden-menu');
-/* Tab-Down */
-var tabDown = document.getElementById('tab-down');
-var tabDownInner = document.getElementById('tab-down-inner');
-/* Tab-Down Search Bar */
-var tabSearchContainer = document.getElementById('tab-search-container');
-var tabSearch = document.getElementById('tab-search');
-/* Tab-Down Sign In Button */
-var tabSign = document.getElementById('tab-sign');
-/* Tab-Down Menu List */
-var tabItems = document.getElementById('tab-items');
-/* Tab-Down Post Button */
-var tabList = document.getElementById('tab-post-container');
-
-var mainMenu = document.getElementById('main-menu');
-var searchToggle = document.getElementById('search');
-var searchContainer = document.getElementById('search-container');
-
 var card = document.getElementsByClassName('card');
 var id = parseInt(card[0].id);
 
@@ -42,10 +21,36 @@ function changeHeight() {
   }
 }
 
-/* Tab Down 표출된 상황에서 VW를 1250px 이상으로 늘릴 시 Tab Down 숨기기 */
 window.addEventListener('resize', () => {
   changeHeight();
-  
+})
+
+window.addEventListener('load', () => {
+  changeHeight();
+})
+
+/* Menu Icon */
+var menuIcon = document.getElementById('menu-icon');
+/* Menu Icon Container */
+var hiddenMenu = document.getElementById('hidden-menu');
+/* Tab-Down */
+var tabDown = document.getElementById('tab-down');
+var tabDownInner = document.getElementById('tab-down-inner');
+/* Tab-Down Search Bar */
+var tabSearchContainer = document.getElementById('tab-search-container');
+var tabSearch = document.getElementById('tab-search');
+/* Tab-Down Sign In Button */
+var tabSign = document.getElementById('tab-sign');
+/* Tab-Down Menu List */
+var tabItems = document.getElementById('tab-items');
+/* Tab-Down Post Button */
+var tabList = document.getElementById('tab-post-container');
+
+var mainMenu = document.getElementById('main-menu');
+var searchToggle = document.getElementById('search');
+var searchContainer = document.getElementById('search-container');
+
+window.addEventListener('resize', () => {
   if (window.innerWidth > 1250) {
     /* Menu Icon 복구 */
     menuIcon.className = 'fas fa-bars';
@@ -60,8 +65,4 @@ window.addEventListener('resize', () => {
     tabItems.setAttribute('style', 'opacity: 0; pointer-events: none; transition: 0;');
     tabList.setAttribute('style', 'opacity: 0; pointer-events: none; transition: 0;');
   }
-})
-
-window.addEventListener('load', () => {
-  changeHeight();
 })
